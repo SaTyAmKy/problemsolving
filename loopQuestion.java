1f6b3ebadf190fd6bc79e2980c5e6292baa6196a
SaTyAmKy <ftsatyamkr2002@gmail.com>
2025-05-20 21:36:57 +0530

    java

---
+++ loopQuestion.java
@@ -0,0 +1,466 @@
+
+public class loopQuestion {
+	public static void main(String[] args) {
+		
+//		Question 1:	
+//		for(int i = 1;i <= 5;i++) {			
+//			for(int j = 1;j <= i;j++) {
+//				System.out.print(j);
+//				}
+//				System.out.println();	
+//		}
+//		Question 2:
+//				for(int i = 1;i <= 5;i++) {			
+//					for(int j = i;j >= 1;j--) {
+//						System.out.print(j);
+//					}
+//					System.out.println();
+//				}
+//		Question 3:
+//			for(int i =1;i <= 5;i++) {
+//				for(int j = 5;j >=i;j--) {
+//					System.out.print(j);
+//				}
+//				System.out.println();
+//			}
+//		Question 4:
+//			for(int i = 1;i <= 5;i++) {
+//				for(int j = i;j <=5;j++) {
+//					System.out.print(j);
+//				}
+//				System.out.println();
+//			}
+//		Question 5: VVI
+//			for(int i = 5;i  >= 1;i--) {
+//				for(int j= 5;j >=  i ;j--) {
+//					System.out.print(j);
+//				}
+//				System.out.println();
+//			}
+//			for(int i = 2;i <= 5;i++) {
+//				for(int j = 5; j >= i;j--) {
+//					System.out.print(j);
+//				}
+//				System.out.println();
+//			}
+//		Question 6:
+//		for(int i = 5;i >= 1;i--) {
+//			for(int j = i;j <= 5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+			
+//		Question 7:
+//		for(int i=1;i <= 5;i++) {
+//			for(int j = 5;j >= i;j--) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//		Question 8:
+//			for(int i = 1; i <= 5;i++) {
+//				for(int j = 1;j <= i;j++) {
+//					System.out.print(j);
+//				}
+//				System.out.println();
+//			}
+//			for(int i = 4;i >= 1;i--) {
+//				for(int j = 1; j <= i;j++) {
+//				System.out.print(j);	
+//				}
+//				System.out.println();		
+//			}
+//		Question 9:
+//			for(int i = 1;i <= 5;i++) {
+//				for(int j = i;j >= 1;j--) {
+//					System.out.print(j);
+//				}
+//				System.out.println();
+//			}
+//			for(int i = 4;i >= 1;i--) {
+//				for(int j = i;j >= 1;j--) {
+//					System.out.print(j);
+//				}
+//				System.out.println();
+//			}
+//		Question 10:
+//		for(int i = 5;i >= 1;i--) {
+//			for(int j = i;j <= 5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		for(int i = 2;i <= 5;i++) {
+//			for(int j = i;j <=5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question 11:
+//		for(int i = 5;i >= 1;i--) {
+//			for(int j = 1;j <= i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		for(int i = 2;i <= 5;i++) {
+//			for(int j = 1;j <= i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question 12:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = 5; j >= i;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		for(int i = 5; i >= 1;i--) {
+//			for(int j = 5;j >=i;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//			
+//		}
+//		Question 13:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = 1; j<= i;j++) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//		Question 14 :
+//		for(int i = 5;i >= 1;i--) {
+//			for(int j = 5;j >= i;j--) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//		Question 15:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = 5;j >= i;j--) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//		Question 16:
+//		for(int i = 5;i >= 1;i--) {
+//			for(int j = 5;j >= i;j--) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//		for(int i = 2;i <= 5;i++) {
+//			for(int j = 5;j >= i;j--) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//		Question 17:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = 1;j <= i;j++) {
+//				System.out.print('*');
+//			}
+//			System.out.println();
+//		}
+//		for(int i = 1; i <= 4;i++) {
+//			for(int j = i;j <= 4;j++) {
+//				System.out.print('*');
+//			}
+//			System.out.println();
+//		}
+//		Question 18:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = i;j <= i+4;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question 19:
+//		int xyz = 0;
+//		for(int i = 1;i <= 4;i++) {
+//			for(int j = 1;j <= i;j++) {
+//				xyz++;
+//				System.out.print(xyz + " ");
+//				
+//			}
+//			System.out.println();
+//		}
+//		Question 20:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = 4;j >= i;j--) {
+//				System.out.print("-");
+//				
+//			}
+//			for(int j= 1;j <= i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question 21:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = 2;j <= i;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = i;j <=5;j++) {
+//				System.out.print(j);
+//			}
+//			
+//			System.out.println();
+//		}
+//		Question 22:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = i;j <= 4;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = i;j >= 1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question 23:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = 2;j <= i;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = 5;j >= i;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question 24: wrong code.			
+//			for(int  i = 5;i >= 1;i--) {
+//				for(int j = 2;j <= i;j++) {
+//					System.out.print("-");
+//				}
+//				System.out.println();
+//			for(int j = 5;j >= i;j--) {
+//				System.out.print(j);
+//			}
+//		Question  25:
+//		for(int i = 5;i >= 1;i--) {
+//			for(int j = i;j <= 4;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = 1;j <= i;j++) {
+//				System.out.print(j);
+//			}				
+//				System.out.println();
+//		Question 26:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j=i;j <= 4;j++) {
+//				System.out.print("-");
+//				
+//			}
+//			for(int j = i;j >= 1;j--) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		Question 27:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = i; j <= 4;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = i;j >= 1;j--) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//			for(int k = 4;k >= 1;k--) {
+//				for(int j = 4;j >= k;j--) {
+//					System.out.print("-");
+//				}
+//				for(int j = k;j >= 1;j--) {
+//					System.out.print(j);
+//				}
+//				System.out.println();
+//			}
+//		Question 28:
+//		for(int i = 5;i >= 1;i--) {
+//			for(int j = i;j >= 2;j--) {
+//				System.out.print("-");
+//			}
+//			for(int j = i;j <= 5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		for(int i = 1;i <= 4;i++) {
+//			for(int j = 2;j <= i;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = i;j <= 5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question 29:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = 2;j <= i;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = i;j <= 5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//								
+//				}
+//		for(int i = 4;i >= 1;i--) {
+//			for(int j = 2;j <= i;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = i ;j <= 5;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question 30:
+//		for(int i = 5; i >= 1;i--) {
+//			for(int j = 2;j <= i;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = i;j <= 5;j++) {
+//				System.out.print('*');
+//			}
+//			System.out.println();
+//		}
+//		for(int i = 1;i <= 4;i++) {
+//			for(int j = 1;j <= i;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = i;j <= 4;j++) {
+//				System.out.print('*');
+//			}
+//			System.out.println();
+//		}
+//		Question 31:
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = i ;j <= 4;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = 1;j <= i;j++) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//		for(int i = 4;i >= 1;i--) {
+//			for(int j = i;j <= 4;j++) {
+//				System.out.print("-");
+//			}
+//			for(int j = i;j >= 1;j--) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//		Question 32:
+//		for(int i = 5;i >= 1;i--) {
+//			for(int j = i;j <= 4;j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j = i; j >= 1;j--) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//		for(int i = 2;i <= 5;i++) {
+//			for(int j = i;j <= 4;j++) {
+//				System.out.print(" ");
+//			}
+//			for(int j = 1;j <= i;j++) {
+//				System.out.print(i);
+//			}
+//			System.out.println();
+//		}
+//		Question 33:
+//		int count = 0;
+//		for(int i = 1;i <= 4;i++) {
+//			for(int j = 3;j >= i;j--) {
+//				System.out.print("-");
+//			}
+//			for(int j = 1;j <= i;j++) {
+//				count++;
+//				System.out.print(count+" ");
+//			}
+//			System.out.println();		
+//		}
+//		Question 34:
+//		for(int i = 0;i <= 4;i++) {
+//			for(int j = 3;j >= i;j--) {
+//				System.out.print("-");
+//			}
+//			for(int j = 1;j <= i*2+1;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question 35:
+//		for(int i = 0;i <= 4;i++) {
+//			for(int j = 3;j >= i;j--) {
+//			System.out.print("-");
+//		}
+//		for(int j = 1;j <= i*2+1;j++) {
+//			System.out.print(j);
+//		}
+//			System.out.println();
+//		}
+//		for(int i =4;i>=1;i-- ) {
+//			for(int j = i;j <= 4;j++) {
+//				System.out.print("-");
+//			}
+//			for (int j = 1;j<=i*2-1;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question:36
+//		for(int i = 0;i <= 4;i++) {
+//		for(int j = 3;j >= i;j--) {
+//		System.out.print("-");
+//	}
+//	for(int j = 1;j <= i*2+1;j++) {
+//		System.out.print('*');
+//	}
+//		System.out.println();
+//	}
+//	for(int i =4;i>=1;i-- ) {
+//		for(int j = i;j <= 4;j++) {
+//			System.out.print("-");
+//		}
+//		for (int j = 1;j<=i*2-1;j++) {
+//			System.out.print('*');
+//		}
+//		System.out.println();
+//	}
+//		Question:37
+//		for(int i = 1;i <= 5;i++) {
+//			for(int j = 4;j >= i;j--) {
+//				System.out.print("-");
+//			}
+//			for(int j = 1;j <= i;j++) {
+//				System.out.print(j);
+//			}
+//			System.out.println();
+//		}
+//		Question:38
+//            int count = 0;
+//		for(int i  = 0;i <= 3;i++) {
+//			for(int j = 3;j >= i+1;j--) {
+//				System.out.print("  ");
+//			}
+//			for(int j = 1;j <=i+1 ;j++) {
+//				count++ ;
+//				System.out.print(" " + count +" ");
+//			}
+//			System.out.println();
+//		}
+
+		
+		
+	}
+}
+		
+		
+	
+
