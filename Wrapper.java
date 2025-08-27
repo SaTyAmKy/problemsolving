122a7c85ac47f64f8a7328384d4b8657f1732a49
SaTyAmKy <ftsatyamkr2002@gmail.com>
2025-08-27 18:31:15 +0530

    java

---
+++ Wrapper.java
@@ -0,0 +1,58 @@
+package javaLangPacakage;
+
+public class Wrapper {
+	public static void main(String[] args) {
+//		Integer I = new Integer(10);
+//		Integer I1 = new Integer("10");
+//		System.out.println(I);
+//		System.out.println(I1);
+		
+//		Double D = new Double(10.5);
+//		Double D1 = new Double("10.5");
+//		System.out.println(D);
+//		System.out.println(D1);
+		
+//		Integer I = new Integer("ten");
+//		System.out.println(I);
+		
+		// Floaat class has three constructor float , double and String.
+//		Float f = new Float(10.5f);
+//		System.out.println(f);
+//		
+//		Float f1 = new Float("10.5f");
+//		System.out.println(f1);
+//		
+//		Float f2 = new Float(10.5);
+//		System.out.println(f2);
+//		
+//		Float f3 = new Float("10.5");
+//		System.out.println(f3);
+		
+		// Character class has only one constructor char argument.
+//		Character ch = new Character('a');
+//		System.out.println(ch);
+		
+//		Boolean B = new Boolean(true);
+//		Boolean B1 = new Boolean(false);
+//		Boolean B2 = new Boolean("durga");
+//		Boolean b3 = new Boolean("True");
+//		System.out.println(B);
+//		System.out.println(B1);
+//		System.out.println(B2);
+//		System.out.println(b3);
+		
+//		Boolean b4 = new Boolean("yes");
+//		Boolean b5 = new Boolean("no");
+//		System.out.println(b4);
+//		System.out.println(b5);
+//		System.out.println(b4.equals(b5));
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
