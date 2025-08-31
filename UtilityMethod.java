e0b082186231b11e9fe50e2c07d41a9890ac306c
SaTyAmKy <ftsatyamkr2002@gmail.com>
2025-08-31 22:13:31 +0530

    java

---
+++ UtilityMethod.java
@@ -0,0 +1,58 @@
+package javaLangPacakage;
+
+public class UtilityMethod {
+	public static void main(String[] args) {
+		
+//		valueOf() methods
+		
+//		Integer i = Integer.valueOf("100",4);
+//		System.out.println(i);
+		
+//		Integer i = Integer.valueOf(10);
+//		System.out.println(i);
+		
+//		Integer i = Integer.valueOf('a');
+//		System.out.println(i);
+		
+//		Boolean B = Boolean.valueOf(true);
+//		System.out.println(B);
+		
+//		Boolean B = Boolean.valueOf(false);
+//		System.out.println(B);
+//		
+//		Character ch = Character.valueOf('s');
+//		System.out.println(ch);
+		
+//		String s  = String.valueOf("s");
+//		System.out.println(s);
+		
+//		xxxValue() methods
+		
+//		Integer i = Integer.valueOf(130);
+//		System.out.println(i.byteValue());
+//		System.out.println(i.shortValue());
+//		System.out.println(i.intValue());
+//		System.out.println(i.longValue());
+//		System.out.println(i.floatValue());
+//		System.out.println(i.doubleValue());
+		
+//		Character ch = new Character('a');
+//		System.out.println(ch);
+//		System.out.println(ch.charValue());
+		
+		Boolean b = Boolean.valueOf("Satyam");
+		System.out.println(b);
+		boolean b1 =b.booleanValue();
+		System.out.println(b1);
+		System.out.println(b == b1);
+		System.out.println(b.equals(b1));
+		
+		
+		
+		
+		
+		
+	}
+
+	
+}
