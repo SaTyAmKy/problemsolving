36d827880b4407118f559b2f74b57393c775c6a7
SaTyAmKy <ftsatyamkr2002@gmail.com>
2025-09-18 23:45:04 +0530

    java

---
+++ IntegerArrayList.java
@@ -0,0 +1,16 @@
+package Collection;
+import java.util.ArrayList;
+public class IntegerArrayList {
+	public static void main(String[] args) {
+		ArrayList<Integer> al = new ArrayList<Integer>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add(40);
+		al.add(50);
+		System.out.println(al);
+		al.remove(2);
+		System.out.println(al);
+	}
+
+}
