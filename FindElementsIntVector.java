d23dc852d0376dc7cebb194d5b39a25984c9e99d
SaTyAmKy <ftsatyamkr2002@gmail.com>
2025-09-27 20:42:33 +0530

    java

---
+++ FindElementsIntVector.java
@@ -0,0 +1,17 @@
+package Collection;
+
+import java.util.*;
+
+public class FindElementsIntVector {
+	public static void main(String[] args) {
+		Vector<Integer> v = new Vector<Integer>();
+		v.addElement(12);
+		v.addElement(7);
+		v.addElement(22);
+		v.addElement(5);
+		v.addElement(18);
+		System.out.println(v.contains(22));
+		
+	}
+
+}
