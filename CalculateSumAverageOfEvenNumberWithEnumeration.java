86b59284d4cb3d63657feb71bfe638a467399f13
SaTyAmKy <ftsatyamkr2002@gmail.com>
2025-10-02 22:36:01 +0530

    java

---
+++ CalculateSumAverageOfEvenNumberWithEnumeration.java
@@ -0,0 +1,28 @@
+package Collection;
+
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class CalculateSumAverageOfEvenNumberWithEnumeration {
+	public static void main(String[] args) {
+		Vector<Integer> v = new Vector<>();
+		v.addAll(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
+		Enumeration<Integer> e = v.elements();
+		double sum = 0;
+		double count= 0;
+		while(e.hasMoreElements()) {
+			Integer i = e.nextElement();
+			if(i%2 ==0) {
+				sum+=i;
+				count++;
+			}
+		}
+		double d = sum/count;
+		System.out.println("Total sum :"+sum);
+		System.out.println("Total count :"+count);
+		System.out.println("Average :"+d);
+
+	}
+
+}
