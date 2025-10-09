255f117b72d69a29efa8b03dac67e945ea8c22ad
SaTyAmKy <ftsatyamkr2002@gmail.com>
2025-10-09 23:47:10 +0530

    java

---
+++ NullInsertionInTreeSet.java
@@ -0,0 +1,9 @@
+package CollectionExamples;
+import java.util.*;
+public class NullInsertionInTreeSet {
+	public static void main(String[] args) {
+		TreeSet t = new TreeSet();
+		t.add(null);
+	}
+
+}
