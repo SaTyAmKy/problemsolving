2254fc36118f1f14f5118ef966c9d6e34b656bcf
SaTyAmKy <ftsatyamkr2002@gmail.com>
2025-10-09 23:47:33 +0530

    java

---
+++ AutomaticSorting.java
@@ -0,0 +1,11 @@
+package CollectionExamples;
+import java.util.*;
+public class AutomaticSorting {
+	public static void main(String[] args) {
+		TreeSet<String> t = new TreeSet<>();
+		t.addAll(Arrays.asList("Ravi", "Amit", "Vijay", "Anu"));
+		System.out.println("Automatic Sorting :"+t);
+		
+	}
+
+}
