2ec44567f5ff23b6648830316103000d82ef8a61
SaTyAmKy <ftsatyamkr2002@gmail.com>
2025-10-15 23:39:21 +0530

    java

---
+++ check.java
@@ -0,0 +1,13 @@
+package checking;
+
+
+public class check {
+	public static void main(String[] args) {
+		        System.out.println("Java Version: " + System.getProperty("java.version"));
+		        System.out.println("Java Home: " + System.getProperty("java.home"));
+		    }
+		
+
+	}
+
+
