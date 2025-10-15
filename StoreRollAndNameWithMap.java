2fbfb488e2b3c1c483eeb4119793f5581657bd2e
SaTyAmKy <ftsatyamkr2002@gmail.com>
2025-10-15 23:40:45 +0530

    java

---
+++ StoreRollAndNameWithMap.java
@@ -0,0 +1,13 @@
+package checking;
+import java.util.*;
+public class StoreRollAndNameWithMap {
+	public static void main(String[] args) {
+		HashMap h = new HashMap();
+		h.put(101,"Satyam");
+		h.put(102,"Aman");
+		h.put(103,"Riya");
+		System.out.println(h);
+		
+	}
+
+}
